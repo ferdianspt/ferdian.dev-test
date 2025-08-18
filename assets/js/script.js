f7bba4b002990a8eeea41921b30ed86a81b7484a
ferdianspt <ferdiansaputro3615@gmail.com>
2025-08-18 20:41:51 +0700

    update portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,14 +38,26 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 
   testimonialsItem[i].addEventListener("click", function () {
 
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+ modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
+modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
+modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
+modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
 
-    testimonialsModalFunc();
+// update tanggal modal
+const dateElem = this.querySelector("[data-testimonials-date]");
+if(dateElem){
+  const modalDateElem = document.querySelector("[data-modal-date]");
+  modalDateElem.innerHTML = dateElem.innerHTML;
+  modalDateElem.setAttribute("datetime", dateElem.getAttribute("datetime"));
+} else {
+  document.querySelector("[data-modal-date]").innerHTML = "";
+}
 
-  });
+
+  testimonialsModalFunc();
+
+});
+
 
 }
 
